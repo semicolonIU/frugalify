3bcd61f4f1d6ec727e8c3a44b6ad1f5d39e1eda4
ahmadnurhelmi-maker <ahmadnurhelmi@gmail.com>
2026-09-14 22:54:25 +0800

    fix: Update Appwrite endpoint to Singapore regional cloud URL

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Databases, Account, ID, Query } from 'appwrite';
 import { CashTransaction, InvestmentAsset, UserSettings, AppUser } from './types';
 
-const APPWRITE_ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1';
+const APPWRITE_ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || 'https://sgp.cloud.appwrite.io/v1';
 const APPWRITE_PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || '';
 const APPWRITE_DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || 'financial_db';
 
